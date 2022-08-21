267ca80936b71723184cdc84da2fc2b5a14c2cd7
leeyawnz <58063567+leeyawnz@users.noreply.github.com>
2022-08-22 02:22:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,44 @@ const calendar = document.getElementById('calendar');
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const monthDisplay = document.getElementById('month-display');
 
-function load() {
+const sheetId = '1C6HH_l-1NznSc364RidqnYrI5oz7IOj5bD-Tp3SPrbM';
+const base = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;
+const sheetName = 'Ops';
+const query = encodeURIComponent('Select *');
+const url = `${base}&sheet=${sheetName}&tq=${query}`;
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+async function events() {
+    const response = await fetch(url).then(response => response.text());
+    const data = await JSON.parse(response.substring(47).slice(0, -2));
+    const eventArray = [];
+
+    const dataCol = data.table.cols;
+    const dataRow = data.table.rows;
+    console.log(dataCol);
+    console.log(dataRow);
+    
+    for (let i = 0; i < dataCol.length; i++) {
+        for (let j = 0; j < dataRow.length; j++) {
+            if (dataRow[j].c[i] !== null) {
+                const dateData = dataRow[j].c[i].f;
+                const dateArray = dateData.split(' ');
+                const day = dateArray[0];
+                const month = months.indexOf(dateArray[1]);
+                const year = dateArray[2];
+                const date = `${month + 1}/${day}/${year}`;
+                let event = `{"date":"${date}","title":"${dataCol[i].label}"}`;
+                event = JSON.parse(event);
+                eventArray.push(event);
+            }
+        }
+    }
+
+    buttons(eventArray);
+    setTimeout(load(eventArray), 5000);
+}
+
+function load(eventArray) {
     const date = new Date();
 
     if (nav !== 0) {
@@ -35,15 +72,22 @@ function load() {
         daySquare.classList.add('day');
 
         const dayString = `${month + 1}/${i - paddingDays}/${year}`;
-        console.log(dayString);
 
         if (i > paddingDays) {
             daySquare.innerText = i - paddingDays;
 
+            for (let i = 0; i < eventArray.length; i++) {
+                if (eventArray[i].date == dayString) {
+                    const event = document.createElement('div');
+                    event.classList.add('event');
+                    event.innerText = eventArray[i].title;
+                    daySquare.appendChild(event);
+                }
+            }
+            
             if (i - paddingDays === day && nav === 0) {
                 daySquare.setAttribute('id', 'current-day');
             }
-
         } else {
             daySquare.classList.add('padding');
         }
@@ -52,18 +96,17 @@ function load() {
     }
 }
 
-function buttons() {
+function buttons(eventArray) {
     const nextButton = document.getElementById('next-button');
     nextButton.addEventListener('click', () => {
         nav++;
-        load();
+        load(eventArray);
     });
     const backButton = document.getElementById('back-button');
     backButton.addEventListener('click', () => {
         nav--;
-        load();
+        load(eventArray);
     });
 }
 
-buttons();
-load();
+events();
