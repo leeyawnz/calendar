e4dd6fcde15ebf2b91241c04634c64f626f3cab1
leeyawnz <58063567+leeyawnz@users.noreply.github.com>
2022-08-21 20:43:07 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,64 @@
+let nav = 0;
+
+const calendar = document.getElementById('calendar');
+const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const monthDisplay = document.getElementById('month-display');
+
+function load() {
+    const date = new Date();
+
+    if (nav !== 0) {
+        date.setMonth(new Date().getMonth() + nav);
+    }
+
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+
+    const firstDayOfMonth = new Date(year, month, 1);
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+    const dateString = firstDayOfMonth.toLocaleDateString('en-SG', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric'
+    })
+    const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+
+    monthDisplay.innerText = `${date.toLocaleDateString('en-SG', {month: 'long'})} ${year}`;
+
+    calendar.innerHTML = '';
+
+    for (let i = 1; i <= paddingDays + daysInMonth; i++) {
+        const daySquare = document.createElement('div');
+        daySquare.classList.add('day');
+
+        const dayString = `${month + 1}/${i - paddingDays}/${year}`;
+        console.log(dayString);
+
+        if (i > paddingDays) {
+            daySquare.innerText = i - paddingDays;
+        } else {
+            daySquare.classList.add('padding');
+        }
+
+        calendar.appendChild(daySquare);
+    }
+}
+
+function buttons() {
+    const nextButton = document.getElementById('next-button');
+    nextButton.addEventListener('click', () => {
+        nav++;
+        load();
+    });
+    const backButton = document.getElementById('back-button');
+    backButton.addEventListener('click', () => {
+        nav--;
+        load();
+    });
+}
+
+buttons();
+load();
